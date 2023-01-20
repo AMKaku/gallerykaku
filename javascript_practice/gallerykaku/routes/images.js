7e15f22bbb53974d565033e4c3d110f679896d2d
Muli <arthurkaku@gmail.com>
2023-01-20 13:10:50 +0300

    create gallery app

---
+++ javascript_practice/gallerykaku/routes/images.js
@@ -0,0 +1,9 @@
+// create image schema
+const imageSchema = new mongoose.Schema({
+    name:String,
+    path:String,
+    date:Date
+});
+
+// create the Image class and export it
+module.exports = new mongoose.model('Image', imageSchema);
